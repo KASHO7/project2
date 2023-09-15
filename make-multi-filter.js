cbe096b8db579412338a148cb92da55d92395343
KASHO7 <96492831+KASHO7@users.noreply.github.com>
2023-09-15 10:55:27 -0400

    worked makemultifilter.js

--- make-multi-filter.js
+++ make-multi-filter.js
@@ -1,14 +1,18 @@
 'use strict';
-function MakeMultiFilter(originalArray){
+function MakeMultiFilter(originalArray) {
     let currentArray = originalArray.slice();
-    function arrayFilterer(filterCriteria, callback){
 
-        if(typeof filterCriteria === 'function'){
+    function arrayFilterer(filterCriteria, callback) {
+        if (typeof filterCriteria === 'function') {
             currentArray = currentArray.filter(filterCriteria);
         }
-        if(typeof callback === 'function'){
-        callback(currentArray);
+
+        if (typeof callback === 'function') {
+            callback(currentArray);
         }
+
         return arrayFilterer;
     }
+
+    return arrayFilterer;
 }
--- template-processor.js
+++ template-processor.js
@@ -1,3 +1,4 @@
+'use strict';
 function TemplateProcessor(template) {
     this.template = template;
 }
