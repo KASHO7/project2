db3c0d35c5da191a9cabd8dfb4cd7939ed7cc2bd
KASHO7 <96492831+KASHO7@users.noreply.github.com>
2023-09-15 11:13:10 -0400

    makemultifilter.js is now working

--- make-multi-filter.js
+++ make-multi-filter.js
@@ -1,17 +1,17 @@
 'use strict';
 function MakeMultiFilter(originalArray) {
-    let currentArray = originalArray.slice();
+    var currentArray = originalArray.slice();
 
     function arrayFilterer(filterCriteria, callback) {
         if (typeof filterCriteria === 'function') {
             currentArray = currentArray.filter(filterCriteria);
+            if (typeof callback === 'function') {
+                callback.call(originalArray, currentArray);
+            }
+            return arrayFilterer;
+        } else {
+            return currentArray;
         }
-
-        if (typeof callback === 'function') {
-            callback(currentArray);
-        }
-
-        return arrayFilterer;
     }
 
     return arrayFilterer;
