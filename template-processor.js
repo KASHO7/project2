58588b5dd87a14500cb2d57a6f3f3e5382489dae
KASHO7 <96492831+KASHO7@users.noreply.github.com>
2023-09-18 13:08:44 -0400

    template-processor.js is now working

--- template-processor.js
+++ template-processor.js
@@ -1,31 +1,18 @@
 'use strict';
-function TemplateProcessor(template) {
-    this.template = template;
+
+class TemplateProcessor {
+    constructor(template) {
+        this.template = template;
+    }
+    fillIn(dictionary){
+        let returnString = this.template;
+        for (const property in dictionary){
+            if (Object.prototype.hasOwnProperty.call(dictionary, property)) {
+                returnString = returnString.replaceAll("{{" + property + "}}",dictionary[property]);
+            }
+        }
+        const regex = /{{.*}}/g;
+        returnString = returnString.replaceAll(regex,"");
+        return returnString;
+    }
 }
-
-TemplateProcessor.prototype.fillIn = function (dictionary) {
-    return this.template.replace(/{{(.*?)}}/g, (match, property) => {
-        return dictionary.hasOwnProperty(property) ? dictionary[property] : '';
-    });
-};
-
-// Example usage:
-var template = 'My favorite month is {{month}} but not the day {{day}} or the year {{year}}';
-var dateTemplate = new TemplateProcessor(template);
-
-var dictionary = { month: 'July', day: '1', year: '2016' };
-var str = dateTemplate.fillIn(dictionary);
-
-console.assert(
-    str === 'My favorite month is July but not the day 1 or the year 2016',
-    'Test 1 Failed'
-);
-
-//Case: property doesn't exist in dictionary
-var dictionary2 = { day: '1', year: '2016' };
-var str2 = dateTemplate.fillIn(dictionary2);
-
-console.assert(
-    str2 === 'My favorite month is  but not the day 1 or the year 2016',
-    'Test 2 Failed'
-);
